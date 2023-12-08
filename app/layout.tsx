547e3f38bfd4a5abbfb550d435aa03f5bc5a1d99
nilsreich <nilsreich@gmail.com>
2023-12-08 18:16:19 +0000

    Refactor layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,17 @@
 import "./globals.css"
-import { Inter as FontSans } from "next/font/google"
 
-import { cn } from "@/lib/utils"
 import React from "react"
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
       <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
+        className=
+        "min-h-screen bg-background font-sans antialiased"
+
       >
         {children}
       </body>
